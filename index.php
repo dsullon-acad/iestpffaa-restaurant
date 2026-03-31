d1188be665e4e91a330732ae732d76bc3ff9610e
Alex Sullon <alexsullonporras@hotmail.com>
2026-03-31 10:23:42 -0500

    Inicio de proyecto

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/app.css">
+    <title>Gourmet Criollo</title>
+</head>
+<body>
+    <div class="layout">
+        <header class="header">
+            <div class="header__contenido container">
+                <p class="header__logo">Gourmet Criollo</p>
+                <nav class="header__nav">
+                    <ul>
+                        <li><a href="#">Inicio</a></li>
+                        <li><a href="#">Carta</a></li>
+                        <li><a href="#">Blog</a></li>
+                        <li><a href="#">Contacto</a></li>
+                    </ul>
+                </nav>
+                <a class="cta-primary" href="#">Reservar Mesa</a>
+            </div>
+        </header>
+        <main>
+            <p>Contenido principal</p>
+        </main>
+        <footer class="footer">
+            <p>Footer</p>
+        </footer>
+    </div>
+</body>
+</html>
