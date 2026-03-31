9733a201fc82b4321ed893a583eb650bb57208c7
Alex Sullon <alex.sullonporras@gmail.com>
2026-03-31 13:13:23 -0500

    feat: se agregó contenido y css a la carta de portada

--- index.php
+++ index.php
@@ -33,7 +33,66 @@
                     </div>
                 </div>
             </section>
-
+            <section class="menu container">
+                <div class="menu__carta">
+                    <h2>Nuestra carta</h2>
+                    <div class="carta__contenido">
+                        <div class="carta__seccion">
+                            <h3>Entrada</h3>
+                            <div class="carta__item">
+                                <p>Causa Limeña</p>
+                                <p>S/ 20.00</p>
+                            </div>
+                            <div class="carta__item">
+                                <p>Papa a la huancaina</p>
+                                <p>S/ 18.00</p>
+                            </div>
+                            <div class="carta__item">
+                                <p>Sopa de pollo</p>
+                                <p>S/ 20.00</p>
+                            </div>
+                        </div>
+                        <div class="carta__seccion">
+                            <h3>Fondo</h3>
+                            <div class="carta__item">
+                                <p>Lomo Saltado</p>
+                                <p>S/ 42.00</p>
+                            </div>
+                            <div class="carta__item">
+                                <p>Tallarin Saltado</p>
+                                <p>S/ 35.00</p>
+                            </div>
+                            <div class="carta__item">
+                                <p>Arroz con pato</p>
+                                <p>S/ 48.00</p>
+                            </div>
+                            <div class="carta__item">
+                                <p>Seco de carne con frejoles</p>
+                                <p>S/ 38.00</p>
+                            </div>
+                        </div>
+                        <div class="carta__seccion">
+                            <h3>Postre</h3>
+                            <div class="carta__item">
+                                <p>Mazamorra morada</p>
+                                <p>S/ 8.00</p>
+                            </div>
+                            <div class="carta__item">
+                                <p>Mazamorra de calabaza</p>
+                                <p>S/ 8.00</p>
+                            </div>
+                            <div class="carta__item">
+                                <p>Arroz con leche</p>
+                                <p>S/ 12.00</p>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+                <div class="menu__historia">
+                    <h2>Sabores del Perú</h2>
+                </div>
+            </section>
         </main>
         <footer class="footer">
             <p>Footer</p>
